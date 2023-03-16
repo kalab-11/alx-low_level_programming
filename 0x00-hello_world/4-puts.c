fb2d4c486d3cacb0e4d5bb2c30af75b264950649
Kalab Gette <israelgette@gmail.com>
2023-03-16 07:15:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - Printing using the puts function
+ * Return: 0 checks passed
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
