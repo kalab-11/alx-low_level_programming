bea15208ee263d4fb1bb07e52f308500bf516c7b
Kalab Gette <israelgette@gmail.com>
2023-03-20 11:44:13 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : prints all single digit numbers from 0 to 9
+ * Return: 0 checks passed
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
