b96822c5448d23a956293d6523704c34a7397caf
Kalab Gette <israelgette@gmail.com>
2023-03-20 11:36:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all numbers from 0 to 9
+ * Return: 0 checks passed
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
