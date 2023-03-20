6b0f2bdfb886b79264f8553b00563275052681bf
Kalab Gette <israelgette@gmail.com>
2023-03-20 04:23:42 -0700

    a program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "printing lowercase alphabet using putchar"
+ * Return: 0 checks passed
+*/
+int main(void)
+{
+	int c = 95;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
